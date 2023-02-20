83d3bbc1f1db18d06f5b315de706d04b79279db6
henigesa <121961371+henigesa@users.noreply.github.com>
2023-02-20 01:22:32 -0500

    with code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        //declare variables
+        int meter = 0;
+        double feet;
+        double mile;
+        double conversionFactor = 1606;
+        double inches;
+        //prompt user to enter the variables
+        System.out.println("Enter the number of meters:");
+        //take the user input
+        if (input.hasNextInt()) {
+            meter = input.nextInt();
+            input.nextLine();
+        } else {
+            System.out.println("That is not a valid input. Run the program and try again.");
+            System.exit(0);
+        }
+        //do math
+        feet = meter * 3.281;
+        mile = meter / conversionFactor;
+        inches = meter * 39.37;
+        System.out.println(meter + " meters is " + feet + " feet.");
+        System.out.println(meter + " meters is " + mile + " miles.");
+        System.out.println(meter + " meters is " + inches + " inches.");
     }
 }
